b9c682b747820ae2cb4934ca343ecde0b0a6d730
LuisPHernandez <lphm0105@gmail.com>
2025-03-27 18:02:36 -0600

    Agregar Javadoc

--- Paciente.java
+++ Paciente.java
@@ -1,33 +1,46 @@
 public class Paciente implements Comparable<Paciente>{
-
+    // Atributos
     private String nombre;
     private String lesion;
     private char prioridad; 
 
-    //getters
+    // Métodos
+    /**
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
+    /**
+     * @return
+     */
     public String getLesion() {
         return lesion;
     }
+    /**
+     * @return
+     */
     public char getPrioridad() {
         return prioridad;
     }
 
-
-
+    /**
+     * @param nombre
+     * @param lesion
+     * @param prioridad
+     */
     public Paciente(String nombre, String lesion, char prioridad) {
         this.nombre = nombre;
         this.lesion = lesion;
         this.prioridad = prioridad;
     }
-        // metodo para comparar al paciente con otro, definido como "Siguente"
 
+    // Método para comparar al paciente con otro, definido como "Siguente"
     public int compareTo(Paciente siguiente){
         return Character.compare(this.prioridad, siguiente.prioridad);
     }
-    //metodo par pasar la info del pacietne como String
+
+    // Método par pasar la info del paciente como String
 
     public String toString(){
         return nombre + "," + lesion + "," + prioridad;
